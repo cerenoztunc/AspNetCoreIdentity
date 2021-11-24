9740dc8f51b6a76c810960dcf5f9cfa17888f515
naciyeceren097 <naciye.ceren097@gmail.com>
2021-11-25 00:48:36 +0300

    Installing Asp.Net core identity

--- Project.MVC/Startup.cs
+++ Project.MVC/Startup.cs
@@ -1,8 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Project.MVC.Models;
+using Project.MVC.Models.DbContext;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +17,20 @@ namespace Project.MVC
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration _configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppIdentityDbContext>(opt =>
+            {
+                opt.UseSqlServer(_configuration["ConnectionStrings:DefaultConnectionString"]);
+            });
+
+            services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<AppIdentityDbContext>();
+
             services.AddMvc(opt=> 
             {
                 opt.EnableEndpointRouting = false;
@@ -29,7 +44,9 @@ namespace Project.MVC
             app.UseDeveloperExceptionPage(); //alýnan hatalarla ilgili açýklayýcý bilgiler sunar..
             app.UseStatusCodePages(); //özellikle herhangi bir content dönmeyen sayfalarda bilgilendirici yazýlar gösterir..
             app.UseStaticFiles();
-            app.UseMvcWithDefaultRoute(); 
+            app.UseMvcWithDefaultRoute();
+
+            app.UseAuthentication();
         }
     }
 }
