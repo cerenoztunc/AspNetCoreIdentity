9c982983b1e76be333ab7d05767643c8d64a98d1
naciyeceren097 <naciye.ceren097@gmail.com>
2021-11-25 12:48:58 +0300

    Adding SignUp process

--- Project.MVC/Controllers/HomeController.cs
+++ Project.MVC/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Project.MVC.Models;
+using Project.MVC.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +11,45 @@ namespace Project.MVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+
+        public HomeController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpViewModel signUpViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser
+                {
+                    UserName = signUpViewModel.UserName,
+                    Email = signUpViewModel.Email,
+                    PhoneNumber = signUpViewModel.PhoneNumber
+                };
+                IdentityResult result = await _userManager.CreateAsync(appUser,signUpViewModel.Password);
+                if (result.Succeeded) return RedirectToAction("LogIn");
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View(signUpViewModel);
+        }
+        public IActionResult LogIn()
         {
             return View();
         }
---
+++ Project.MVC/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.MVC.ViewModels
+{
+    public class SignUpViewModel
+    {
+        [Required(ErrorMessage ="{0} is required!")]
+        public string UserName { get; set; }
+        public string PhoneNumber { get; set; }
+        [Required(ErrorMessage ="{0} is required!")]
+        [EmailAddress(ErrorMessage = "{0} is not in the correct format!")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "{0} is required!")]
+        [DataType(DataType.Password)]
+        [MinLength(3,ErrorMessage = "{0}, must be at least {1} characters!")]
+        public string Password { get; set; }
+    }
+}
