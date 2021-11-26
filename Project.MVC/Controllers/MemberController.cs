507d1f9b5f44893eb81e5bfdba164e1cff58a1ac
naciyeceren097 <naciye.ceren097@gmail.com>
2021-11-26 13:08:53 +0300

    Designing the user home page

--- Project.MVC/Controllers/MemberController.cs
+++ Project.MVC/Controllers/MemberController.cs
@@ -1,18 +1,31 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Project.MVC.Models;
+using Project.MVC.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Mapster;
 namespace Project.MVC.Controllers
 {
     [Authorize]
     public class MemberController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<AppUser> _userManager;
+
+        public MemberController(UserManager<AppUser> userManager)
         {
-            return View();
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            AppUser user = await _userManager.FindByNameAsync(User.Identity.Name); //her kullanıcı için bir User.Identity yapısı mutlaka oluşur. kullanıcı kayıt yapmamış olsa da ama burada içi boştur. Eğer kullanıcı kayıt yapmışsa o zaman bilgiler dolmaya başlar..
+            UserViewModel userViewModel = user.Adapt<UserViewModel>(); //adapt map gibidir. ondan daha hafiftir. 
+            
+            return View(userViewModel);
         }
     }
 }
---
+++ Project.MVC/ViewModels/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.MVC.ViewModels
+{
+    public class UserViewModel:SignUpViewModel
+    {
+    }
+}
