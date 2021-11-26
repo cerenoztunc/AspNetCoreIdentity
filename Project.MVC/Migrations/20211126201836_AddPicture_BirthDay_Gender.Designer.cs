5be511a8b854a711b730d1028f75091dfe3cc3c6
naciyeceren097 <naciye.ceren097@gmail.com>
2021-11-26 23:22:30 +0300

    Adding new columns to the user table

--- Project.MVC/Migrations/20211126201836_AddPicture_BirthDay_Gender.Designer.cs
+++ Project.MVC/Migrations/20211126201836_AddPicture_BirthDay_Gender.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Project.MVC.Models.DbContext;
 
 namespace Project.MVC.Migrations
 {
     [DbContext(typeof(AppIdentityDbContext))]
-    partial class AppIdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211126201836_AddPicture_BirthDay_Gender")]
+    partial class AddPicture_BirthDay_Gender
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Project.MVC/Models/AppUser.cs
+++ Project.MVC/Models/AppUser.cs
@@ -8,6 +8,9 @@ namespace Project.MVC.Models
 {
     public class AppUser:IdentityUser
     {
-        
+        public string City { get; set; }
+        public string Picture { get; set; }
+        public DateTime? BirthDay { get; set; }
+        public int Gender { get; set; }
     }
 }
