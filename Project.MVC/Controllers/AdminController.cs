9cd00578cf712860d0a89f087298f09000a1abe7
naciyeceren097 <naciye.ceren097@gmail.com>
2021-11-28 02:48:25 +0300

    Adding create role process

--- Project.MVC/Controllers/AdminController.cs
+++ Project.MVC/Controllers/AdminController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Project.MVC.Models;
+using Project.MVC.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +22,27 @@ namespace Project.MVC.Controllers
         public IActionResult Users()
         {
             return View(_userManager.Users.ToList());
+        }
+        public IActionResult RoleCreate()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult RoleCreate(RoleViewModel roleViewModel)
+        {
+            AppRole appRole = new AppRole
+            {
+                Name = roleViewModel.Name
+            };
+            IdentityResult result = _roleManager.CreateAsync(appRole).Result;
+            if (result.Succeeded)
+                return RedirectToAction("Roles");
+            else AddModelError(result);
+            return View(roleViewModel);
+        }
+        public IActionResult Roles()
+        {
+            return View(_roleManager.Roles.ToList());
         }
     }
 }
---
+++ Project.MVC/ViewModels/RoleViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.MVC.ViewModels
+{
+    public class RoleViewModel
+    {
+        [Display(Name="User Name")]
+        [Required(ErrorMessage ="{0} is required!")]
+        public string Name { get; set; }
+        public string Id { get; set; }
+    }
+}
